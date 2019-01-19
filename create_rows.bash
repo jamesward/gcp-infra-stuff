5e27fe111af4b46911478e3905ba60bdb814a581
amiteinav <38571744+amiteinav@users.noreply.github.com>
2019-01-19 10:09:58 +0200

    Add files via upload

---
+++ create_rows.bash
@@ -0,0 +1,7 @@
+
+#!/bin/bash 
+COUNTER=0
+while [  $COUNTER -lt $1 ]; do
+	echo "sdfsdfsdf,sdfsdfsdfsdfs,fsdfsdff,sdfsdfsdf,sdfsdfsdfsdf,sdfsdfsdfsdfs,dfsfsdfsdfsd,fsdf,sdfsdfsdfsdfsdf,sdfsdfsdfs,dfsdfsdfds" >> file
+	let COUNTER=COUNTER+1 
+done
