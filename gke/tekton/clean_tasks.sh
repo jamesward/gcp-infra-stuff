1b6fc5f61c9b3932f65c01ddff9610dafdd009de
Amit Einav <amiteinav@google.com>
2019-08-13 21:31:19 +0300

    Create clean_tasks.sh

---
+++ gke/tekton/clean_tasks.sh
@@ -0,0 +1,13 @@
+kubectl delete PipelineResource pdf-image-amiteinav
+kubectl delete PipelineResource pdf-git
+kubectl delete Task build-push-kaniko
+kubectl delete taskrun build-push-run
+kubectl delete task deploy-using-kubectl
+kubectl delete task source-to-image
+kubectl delete pipeline build-and-deploy-pipeline
+kubectl delete pipeline build-pipeline
+kubectl delete pipelineresources skaffold-image-leeroy-web
+kubectl delete pipelineresources skaffold-git
+kubectl delete tasks build-push-run
+kubectl delete taskrun build-push-run
+
