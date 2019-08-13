350c2a980889f0a8740e26ff66815e4304babcc6
Amit Einav <amiteinav@google.com>
2019-08-13 17:05:44 +0300

    Delete create_rows.bash

--- create_rows.bash
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash 
-COUNTER=0
-while [  $COUNTER -lt $1 ]; do
-	echo "sdfsdfsdf,sdfsdfsdfsdfs,fsdfsdff,sdfsdfsdf,sdfsdfsdfsdf,sdfsdfsdfsdfs,dfsfsdfsdfsd,fsdf,sdfsdfsdfsdfsdf,sdfsdfsdfs,dfsdfsdfds" >> file
-	let COUNTER=COUNTER+1 
-done
