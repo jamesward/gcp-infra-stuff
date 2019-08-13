553e01db4e16090db899073a9b5266e69bc86ce0
Amit Einav <amiteinav@google.com>
2019-08-13 17:07:33 +0300

    Create enable_data_apis.bash

---
+++ res-mgmt/enable_data_apis.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This should enable all APIs that are relevant to data products
+
+export PROJECT=$DEVSHELL_PROJECT_ID
+gcloud services enable --project $PROJECT composer.googleapis.com &
+gcloud services enable --project $PROJECT bigtable.googleapis.com &
+gcloud services enable --project $PROJECT bigtableadmin.googleapis.com &
+gcloud services enable --project $PROJECT compute.googleapis.com &
+gcloud services enable --project $PROJECT bigquery-json.googleapis.com &
+gcloud services enable --project $PROJECT dataproc.googleapis.com &
+gcloud services enable --project $PROJECT cloudmonitoring.googleapis.com &
+gcloud services enable --project $PROJECT pubsub.googleapis.com &
+gcloud services enable --project $PROJECT storage-api.googleapis.com &
+gcloud services enable --project $PROJECT dataflow.googleapis.com &
+gcloud services enable --project $PROJECT drive.googleapis.com &
+gcloud services enable --project $PROJECT stackdriver.googleapis.com &
+gcloud services enable --project $PROJECT logging.googleapis.com &
+gcloud services enable --project $PROJECT monitoring.googleapis.com &
+gcloud services enable --project $PROJECT spanner.googleapis.com &
+gcloud services enable --project $PROJECT datafusion.googleapis.com &
+gcloud services enable --project $PROJECT container.googleapis.com  &                                              
+gcloud services enable --project $PROJECT containeranalysis.googleapis.com    &                                   
+gcloud services enable --project $PROJECT containerregistry.googleapis.com     &                                   
+gcloud services enable --project $PROJECT containerscanning.googleapis.com                                        
+gcloud services enable --project $PROJECT proxy-cloudbuild.sandbox.googleapis.com    &                             
+
