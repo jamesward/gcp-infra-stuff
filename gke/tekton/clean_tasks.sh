305b0b05a1430576fd1ede7517323034f30228e9
Amit Einav <amiteinav@google.com>
2019-08-14 00:16:47 +0300

    Update clean_tasks.sh

--- gke/tekton/clean_tasks.sh
+++ gke/tekton/clean_tasks.sh
@@ -1,13 +1,13 @@
-kubectl delete PipelineResource pdf-image-amiteinav
-kubectl delete PipelineResource pdf-git
-kubectl delete Task build-push-kaniko
-kubectl delete taskrun build-push-run
-kubectl delete task deploy-using-kubectl
-kubectl delete task source-to-image
-kubectl delete pipeline build-and-deploy-pipeline
-kubectl delete pipeline build-pipeline
-kubectl delete pipelineresources skaffold-image-leeroy-web
-kubectl delete pipelineresources skaffold-git
-kubectl delete tasks build-push-run
-kubectl delete taskrun build-push-run
+kubectl delete PipelineResource pdf-image-amiteinav > /dev/null 2&>1
+kubectl delete PipelineResource pdf-git > /dev/null 2&>1
+kubectl delete Task build-push-kaniko > /dev/null 2&>1
+kubectl delete taskrun build-push-run > /dev/null 2&>1
+kubectl delete task deploy-using-kubectl > /dev/null 2&>1
+kubectl delete task source-to-image > /dev/null 2&>1
+kubectl delete pipeline build-and-deploy-pipeline > /dev/null 2&>1
+kubectl delete pipeline build-pipeline > /dev/null 2&>1
+kubectl delete pipelineresources skaffold-image-leeroy-web > /dev/null 2&>1
+kubectl delete pipelineresources skaffold-git > /dev/null 2&>1
+kubectl delete tasks build-push-run > /dev/null 2&>1
+kubectl delete taskrun build-push-run > /dev/null 2&>1
 
