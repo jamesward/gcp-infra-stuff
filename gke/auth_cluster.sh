5911b7e04caaa6c9d888d77bb3cbf844ff3cd70a
Amit Einav <amiteinav@google.com>
2019-08-14 17:22:24 +0300

    Update auth_cluster.sh

--- gke/auth_cluster.sh
+++ gke/auth_cluster.sh
@@ -1,10 +1,18 @@
+#!/bin/bash
+
+# optional to use $1 to provide a cluster name
+
 export PROJECT_ID=`gcloud config get-value project`
-export CLUSTER=`gcloud container clusters list --format="value(name)" --filter="name:${PROJECT_ID}-cluster"`
-export ZONE=`gcloud container clusters list --format="value(zone)" --filter="name:${PROJECT_ID}-cluster"`
+if [ "${1}" == "" ] ; then
+	export CLUSTER=`gcloud container clusters list --format="value(name)" --filter="name:${PROJECT_ID}-cluster"`
+else
+	export CLUSTER=$1
+fi
+
+export ZONE=`gcloud container clusters list --format="value(zone)" --filter="name:${CLUSTER}"`
 
 gcloud config set project $PROJECT_ID
 gcloud config set container/cluster $CLUSTER
 
 echo "running : gcloud container clusters get-credentials $CLUSTER --zone $ZONE "
 gcloud container clusters get-credentials $CLUSTER --zone $ZONE 
-
