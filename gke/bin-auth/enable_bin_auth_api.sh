b17453b24926a23f26702eb19ede94ebaeebe8bc
Amit Einav <amiteinav@google.com>
2019-08-27 18:01:03 +0300

    Update enable_bin_auth_api.sh

--- gke/bin-auth/enable_bin_auth_api.sh
+++ gke/bin-auth/enable_bin_auth_api.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+#enable the API
 gcloud services enable binaryauthorization.googleapis.com
+
+
+export PROJECT_ID=`gcloud config get-value project`
+if [ "${1}" == "" ] ; then
+	export CLUSTER=`gcloud container clusters list --format="value(name)" --filter="name:${PROJECT_ID}-cluster"`
+else
+	export CLUSTER=$1
+fi
+
+export ZONE=`gcloud container clusters list --format="value(zone)" --filter="name:${CLUSTER}"`
+
+#update the cluster to use Binary Authorization
+gcloud beta container clusters update $CLUSTER \
+--enable-binauthz \
+--zone $ZONE \
 
