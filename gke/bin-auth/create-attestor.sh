bc9642ca3446ee07e1f83b114f6ec17b0cb36300
Amit Einav <amiteinav@google.com>
2019-08-27 18:18:28 +0300

    Update create-attestor.sh

--- gke/bin-auth/create-attestor.sh
+++ gke/bin-auth/create-attestor.sh
@@ -13,3 +13,7 @@ echo "gcloud beta container binauthz attestors create ${ATTESTOR} \
 gcloud beta container binauthz attestors create ${ATTESTOR} \
 --attestation-authority-note=${NOTE_ID} \
 --attestation-authority-note-project=${PROJECT_ID}
+
+# Verify that the attestor was created
+echo "gcloud beta container binauthz attestors list"
+gcloud beta container binauthz attestors list
