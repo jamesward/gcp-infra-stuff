74dc9d531195f9417ce5197af45fa121b5a82af8
Amit Einav <amiteinav@google.com>
2019-08-27 19:30:52 +0300

    Update import-policy.sh

--- gke/bin-auth/import-policy.sh
+++ gke/bin-auth/import-policy.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-echo "gcloud beta container binauthz policy import example-policy.yaml"
-gcloud beta container binauthz policy import ~/gcp-infra-stuff/gke/bin-auth/example-policy.yaml
+source variables
 
+cat > /tmp/policy.yaml << EOM
+    admissionWhitelistPatterns:
+    - namePattern: gcr.io/google_containers/*
+    - namePattern: gcr.io/google-containers/*
+    - namePattern: k8s.gcr.io/*
+    - namePattern: gcr.io/stackdriver-agents/*
+    defaultAdmissionRule:
+      evaluationMode: REQUIRE_ATTESTATION
+      enforcementMode: ENFORCED_BLOCK_AND_AUDIT_LOG
+      requireAttestationsBy:
+        - projects/${PROJECT_ID}/attestors/${ATTESTOR}
+    name: projects/${PROJECT_ID}/policy
+EOM
+
+echo "gcloud beta container binauthz policy import example-policy.yaml"
+gcloud beta container binauthz policy import /tmp/policy.yaml
+
+echo "gcloud beta container binauthz policy export"
+gcloud beta container binauthz policy export
