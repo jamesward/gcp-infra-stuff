c51dee348e49064539263be06dff8a20809ba8c8
Amit Einav <amiteinav@google.com>
2019-08-27 19:55:37 +0300

    Update test-policy.sh

--- gke/bin-auth/test-policy.sh
+++ gke/bin-auth/test-policy.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-kubectl run hello-server --image gcr.io/google-samples/hello-app:1.0 --port 8080
+kubectl run hello-server --generator=run-pod/v1 --image gcr.io/google-samples/hello-app:1.0 --port 8080
 
-sleep 2
 
-kubectl get pods | grep hello-server
 
-sleep 2
-
+# to get last messages
 kubectl get event --template \
 '{{range.items}}{{"\033[0;36m"}}{{.reason}}:{{"\033[0m"}}\{{.message}}{{"\n"}}{{end}}'
-
-sleep 2
-
-kubectl delete deployment hello-server
 
