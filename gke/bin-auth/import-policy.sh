0e8f49d7368f30f4516c6c3ba6e3d3d8dd75a245
Amit Einav <amiteinav@google.com>
2019-08-27 19:27:22 +0300

    Create import-policy.sh

---
+++ gke/bin-auth/import-policy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "gcloud beta container binauthz policy import example-policy.yaml"
+gcloud beta container binauthz policy import ~/gcp-infra-stuff/gke/bin-auth/example-policy.yaml
+
