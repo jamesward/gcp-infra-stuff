939709d0baa55cffd31723eed634edc0a5b5f1ad
Amit Einav <amiteinav@google.com>
2019-08-27 20:33:41 +0300

    Update test-policy.sh

--- gke/bin-auth/test-policy.sh
+++ gke/bin-auth/test-policy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-kubectl run hello-server --generator=run-pod/v1 --image gcr.io/google-samples/hello-app:1.0 --port 8080
+IMAGE_PATH="gcr.io/google-samples/hello-app"
+IMAGE_DIGEST="sha256:c62ead5b8c15c231f9e786250b07909daf6c266d0fcddd93fea882eb722c3be4"
 
+#kubectl run hello-server --generator=run-pod/v1 --image gcr.io/google-samples/hello-app:1.0 --port 8080
 
+kubectl run hello-server --generator=run-pod/v1 --image ${IMAGE_PATH}@${IMAGE_DIGEST} --port 8080
 
 # to get last messages
 kubectl get event --template \
