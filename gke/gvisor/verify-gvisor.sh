04e00a41bb2492c1f9c4eb7aeb29183299164a96
Amit Einav <amiteinav@google.com>
2019-08-27 22:38:52 +0300

    Create verify-gvisor.sh

---
+++ gke/gvisor/verify-gvisor.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "kubectl get mutatingwebhookconfiguration gvisor-admission-webhook-config"
+kubectl get mutatingwebhookconfiguration gvisor-admission-webhook-config"
+
+echo "kubectl get runtimeclass gvisor"
+kubectl get runtimeclass gvisor"
