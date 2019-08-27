8b68cf4abae74e876749129034759f759835afd9
Amit Einav <amiteinav@google.com>
2019-08-27 19:27:15 +0300

    Update clean-keys.sh

--- gke/bin-auth/clean-keys.sh
+++ gke/bin-auth/clean-keys.sh
@@ -13,3 +13,13 @@ gpg --delete-keys "${ATTESTOR}@example.com"
 
 gpg --delete-secret-keys "${ATTESTOR}@example.com"
 gpg --delete-keys "${ATTESTOR}@example.com"
+
+
+PUBKEY=`gcloud beta container binauthz attestors  describe test-attestor | grep "id:" | tail -1 | awk '{print $2}'``
+gcloud beta container binauthz attestors public-keys remove $PUBKEY --attestor=test-attestor
+
+PUBKEY=`gcloud beta container binauthz attestors  describe test-attestor | grep "id:" | tail -1 | awk '{print $2}'``
+gcloud beta container binauthz attestors public-keys remove $PUBKEY --attestor=test-attestor
+
+PUBKEY=`gcloud beta container binauthz attestors  describe test-attestor | grep "id:" | tail -1 | awk '{print $2}'``
+gcloud beta container binauthz attestors public-keys remove $PUBKEY --attestor=test-attestor
