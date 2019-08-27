601239a31c64f829335b283ef42254c471ea3c60
Amit Einav <amiteinav@google.com>
2019-08-27 19:00:10 +0300

    Update set-up-pgp-keys.sh

--- gke/bin-auth/set-up-pgp-keys.sh
+++ gke/bin-auth/set-up-pgp-keys.sh
@@ -14,3 +14,6 @@ EOF
 
 export FINGERPRINT=`gpg --list-keys "${ATTESTOR}@example.com" | grep pub -A1 | grep -v pub | tail -1 | awk '{print $1}'`
 
+# Cleanup commands
+#gpg --delete-keys "test-attestor@example.com"
+#gpg --delete-secret-keys "test-attestor@example.com"
